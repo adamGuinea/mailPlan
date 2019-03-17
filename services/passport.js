496bde14386e037e341dfe05dd54beea012bb4ce
adamGuinea <adamguinea22@gmail.com>
2019-03-18 12:28:33 +1300

    installed react-app and set up routing

--- services/passport.js
+++ services/passport.js
@@ -20,7 +20,8 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
+      callbackURL: "/auth/google/callback",
+      proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
       // first find out if user already exists
