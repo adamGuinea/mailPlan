801fb090f93176cda7968debb53fd82f6a1e7eef
adamGuinea <adamguinea22@gmail.com>
2019-04-06 22:01:18 +1300

    animating homepage component

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,14 +1,28 @@
 import React from "react";
+import styled, { keyframes } from "styled-components";
+import { bounceInLeft, flipInX } from "react-animations";
+
+const Bounce = styled.div`
+  animation: 1s ${keyframes`${bounceInLeft}`} backwards 1s;
+`;
+
+const FlipIn = styled.div`
+  animation: 1.5s ${keyframes`${flipInX}`};
+`;
 
 const Landing = () => {
   return (
     <div className="home-hero">
       <div>
-        <h1 style={{ fontFamily: '"Pacifico", cursive' }}>MailPlan</h1>
-        <p style={{ fontSize: "16px" }}>
-          Create your own email marketing campaign and collect feedback from
-          your users
-        </p>
+        <FlipIn>
+          <h1 style={{ fontFamily: '"Pacifico", cursive' }}>MailPlan</h1>
+        </FlipIn>
+        <Bounce>
+          <p style={{ fontSize: "16px" }}>
+            Create your own email marketing campaign and collect feedback from
+            your users
+          </p>
+        </Bounce>
       </div>
     </div>
   );
