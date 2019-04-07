30e1b9fd69455a2a4874a08ce74c26b17536576f
adamGuinea <adamguinea22@gmail.com>
2019-04-07 20:00:14 +1200

    added a loader

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled, { keyframes } from "styled-components";
 import { bounceInLeft, flipInX } from "react-animations";
+import Loader from "./Loader";
 
 const Bounce = styled.div`
   animation: 1s ${keyframes`${bounceInLeft}`} backwards 1s;
@@ -10,22 +11,46 @@ const FlipIn = styled.div`
   animation: 1.5s ${keyframes`${flipInX}`};
 `;
 
-const Landing = () => {
-  return (
-    <div className="home-hero">
-      <div>
-        <FlipIn>
-          <h1 style={{ fontFamily: '"Pacifico", cursive' }}>MailPlan</h1>
-        </FlipIn>
-        <Bounce>
-          <p style={{ fontSize: "16px" }}>
-            Create your own email marketing campaign and collect feedback from
-            your users
-          </p>
-        </Bounce>
-      </div>
-    </div>
-  );
-};
+class Landing extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ loading: false });
+    }, 2000);
+  }
+
+  render() {
+    let content;
+
+    this.state.loading
+      ? (content = (
+          <div>
+            <Loader />
+          </div>
+        ))
+      : (content = (
+          <div className="home-hero">
+            <div>
+              <FlipIn>
+                <h1 style={{ fontFamily: '"Pacifico", cursive' }}>MailPlan</h1>
+              </FlipIn>
+              <Bounce>
+                <p style={{ fontSize: "16px" }}>
+                  Create your own email marketing campaign and collect feedback
+                  from your users
+                </p>
+              </Bounce>
+            </div>
+          </div>
+        ));
+    return content;
+  }
+}
 
 export default Landing;
