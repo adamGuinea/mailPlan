3ee11b6f1d59472cc7fa3c16fed210b71b1baa9b
adamGuinea <adamguinea22@gmail.com>
2019-04-11 20:32:51 +1200

    added information section

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { FETCH_USER, FETCH_SURVEYS } from "./types";
 
+// keeping my app awake
 setInterval(function() {
   axios.get("https://mailplan.herokuapp.com");
 }, 300000);
--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -12,12 +12,9 @@ const FlipIn = styled.div`
 `;
 
 class Landing extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loading: true
-    };
-  }
+  state = {
+    loading: true
+  };
 
   componentDidMount() {
     setTimeout(() => {
@@ -25,31 +22,83 @@ class Landing extends React.Component {
     }, 2000);
   }
 
-  render() {
-    let content;
-
-    this.state.loading
-      ? (content = (
+  renderHero() {
+    if (this.state.loading) return <Loader />;
+    return (
+      <React.Fragment>
+        <section className="home-hero">
           <div>
-            <Loader />
+            <FlipIn>
+              <h1 style={{ fontFamily: '"Pacifico", cursive' }}>MailPlan</h1>
+            </FlipIn>
+            <Bounce>
+              <p style={{ fontSize: "16px" }}>
+                Create your own email marketing campaign and collect feedback
+                from your users
+              </p>
+            </Bounce>
           </div>
-        ))
-      : (content = (
-          <div className="home-hero">
-            <div>
-              <FlipIn>
-                <h1 style={{ fontFamily: '"Pacifico", cursive' }}>MailPlan</h1>
-              </FlipIn>
-              <Bounce>
-                <p style={{ fontSize: "16px" }}>
-                  Create your own email marketing campaign and collect feedback
-                  from your users
-                </p>
-              </Bounce>
+          <a href="#about" className="white-text">
+            <i className="material-icons medium scroll-icon">
+              arrow_drop_down_circle
+            </i>
+          </a>
+        </section>
+
+        <section className="section">
+          <div className="container">
+            <div className="row scrollspy" id="about">
+              <div className="col s12 m4">
+                <div className="card-panel">
+                  <i className="fa fa-user fa-3x deep-purple-text text-darken-2" />
+                  <h5 className="grey-text text-darken-4">Free Account</h5>
+                  <p>
+                    Create an account and start using MailPlan now to boost your
+                    product sales.
+                  </p>
+                </div>
+              </div>
+              <div className="col s12 m4">
+                <div className="card-panel">
+                  <i className="fa fa-database fa-3x deep-purple-text text-darken-2" />
+                  <h5 className="grey-text text-darken-4">NoSQL Databases</h5>
+                  <p>
+                    We use MongoDB for large volumes of structured and
+                    non-structured data.
+                  </p>
+                </div>
+              </div>
+              <div className="col s12 m4">
+                <div className="card-panel">
+                  <i className="fa fa-envelope fa-3x deep-purple-text text-darken-2" />
+                  <h5 className="grey-text text-darken-4">Fast Connections</h5>
+                  <p>
+                    We use SendGrid API trusted by developers and marketers for
+                    time-savings and delivery expertise.
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
-        ));
-    return content;
+        </section>
+      </React.Fragment>
+    );
+  }
+
+  // renderInfo() {
+  //   return (
+
+  //   );
+  // }
+
+  render() {
+    return (
+      <React.Fragment>
+        {this.renderHero()}
+
+        {/* {this.renderInfo()} */}
+      </React.Fragment>
+    );
   }
 }
 
